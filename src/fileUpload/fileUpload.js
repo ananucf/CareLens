2c8db4cca2b20666e2bcee510f5baaeafbff79f1
ananucf <ananmimucf@gmail.com>
2025-05-10 17:52:00 +0300

    Fix/update

--- src/fileUpload/fileUpload.js
+++ src/fileUpload/fileUpload.js
@@ -1,10 +1,67 @@
+// import multer from 'multer';
+// import { v4 as uuidv4 } from 'uuid';
+// import { AppError } from "../utils/appError.js";
+// import fs from 'fs';
+// import path from 'path';
+
+// // create the folder if it is not exist
+// const ensureFolderExists = (folderPath) => {
+//   if (!fs.existsSync(folderPath)) {
+//     fs.mkdirSync(folderPath, { recursive: true });
+//   }
+// };
+
+// const fileUpload = (folderName) => {
+//   const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       const uploadPath = path.join('uploads', folderName);
+      
+//       // ensure that folder is exist
+//       ensureFolderExists(uploadPath);
+      
+//       cb(null, uploadPath);
+//     },
+//     filename: (req, file, cb) => {
+//       cb(null, uuidv4() + "_" + file.originalname);
+//     }
+//   });
+
+//   function fileFilter(req, file, cb) {
+//     if (file.mimetype.startsWith('image')) {
+//       cb(null, true);
+//     } else {
+//       cb(new AppError('Image only', 401, false));
+//     }
+//   }
+
+//   const upload = multer({
+//     storage,
+//     fileFilter,
+//     limits: {
+//       fileSize: 1 * 2024 * 2024 // 1MB
+//     }
+//   });
+
+//   return upload;
+// };
+
+// export const uploadSingleFile = (fieldName, folderName) => fileUpload(folderName).single(fieldName);
+// export const uploadMixOfFiles = (arrayOfFields, folderName) => fileUpload(folderName).fields(arrayOfFields);
+
+
+
+
+
+
+
+
 import multer from 'multer';
 import { v4 as uuidv4 } from 'uuid';
 import { AppError } from "../utils/appError.js";
 import fs from 'fs';
 import path from 'path';
 
-// create the folder if it is not exist
+// Create the folder if it does not exist
 const ensureFolderExists = (folderPath) => {
   if (!fs.existsSync(folderPath)) {
     fs.mkdirSync(folderPath, { recursive: true });
@@ -15,10 +72,7 @@ const fileUpload = (folderName) => {
   const storage = multer.diskStorage({
     destination: (req, file, cb) => {
       const uploadPath = path.join('uploads', folderName);
-      
-      // ensure that folder is exist
       ensureFolderExists(uploadPath);
-      
       cb(null, uploadPath);
     },
     filename: (req, file, cb) => {
@@ -30,7 +84,7 @@ const fileUpload = (folderName) => {
     if (file.mimetype.startsWith('image')) {
       cb(null, true);
     } else {
-      cb(new AppError('Image only', 401, false));
+      cb(new AppError('Only image files are allowed', 401, false));
     }
   }
 
@@ -38,7 +92,7 @@ const fileUpload = (folderName) => {
     storage,
     fileFilter,
     limits: {
-      fileSize: 1 * 2024 * 2024 // 1MB
+      fileSize: 1 * 1024 * 1024 // 1MB
     }
   });
 
