4264c8fa8e4ef2fa7880dc1c2900571eed284d54
ananucf <ananmimucf@gmail.com>
2025-05-10 17:55:39 +0300

    Fix/update

--- src/modules/scan/scan.routes.js
+++ src/modules/scan/scan.routes.js
@@ -23,6 +23,6 @@ import { scanProductImage } from './scan.controller.js';
 const scanRouter = Router();
 
 // رفع الصورة و تمريرها لتحليل AI
-scanRouter.post('/image', verifyToken, uploadSingleFile('file', 'products'), scanProductImage);
+scanRouter.post('/image', verifyToken, uploadSingleFile('image', 'products'), scanProductImage);
 
 export default scanRouter;
